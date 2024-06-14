1e07565ccc215f44ffd3b82a218f4874483d6885
DonatoAletta <donatoalettawork@gmail.com>
2024-06-14 10:52:38 +0200

    Esercizio su Incapsulamento e Modificatori

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Auto auto = new Auto("Fiat","Panda" ,"PD 336EM", 1200);
+        getterOutput(auto);
+
+
+}
+    public static void getterOutput(Auto auto){
+        System.out.println("Marca: " + auto.getMarca());
+        System.out.println("Modello: " + auto.getModello());
+        System.out.println("Targa: " + auto.getTarga());
+        System.out.println("Cilindrata: " + auto.getCilindrata());
+    }
+
+}
+
